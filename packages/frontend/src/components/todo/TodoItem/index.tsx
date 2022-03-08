89ffd0e6b9098d25435dc906a8685de4b5708910
shitakemura <shitakemura@gmail.com>
2022-03-08 13:58:14 +0900

    feat(frontend): implement handleToggleTodo, handleDeleteTodo using recoil

--- packages/frontend/src/components/todo/TodoItem/index.tsx
+++ packages/frontend/src/components/todo/TodoItem/index.tsx
@@ -1,12 +1,34 @@
 import { Checkbox, HStack, IconButton, Text } from "@chakra-ui/react";
 import { DeleteIcon } from "@chakra-ui/icons";
 import { Todo } from "@/shared/models/todo";
+import { useRecoilState } from "recoil";
+import { todoListState } from "src/store/todo";
+import { useCallback } from "react";
 
 type Props = {
   todo: Todo;
 };
 
-const TodoItem = ({ todo: { id, title, completed } }: Props) => {
+const TodoItem = ({ todo }: Props) => {
+  const { id, title, completed } = todo;
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+
+  const handleToggleTodo = useCallback(() => {
+    const newList = todoList.map((item) => {
+      if (item.id === id) {
+        return { ...item, completed: !item.completed };
+      } else {
+        return item;
+      }
+    });
+    setTodoList(newList);
+  }, [id, todoList, setTodoList]);
+
+  const handleDeleteTodo = useCallback(() => {
+    const newList = todoList.filter((item) => item.id !== id);
+    setTodoList(newList);
+  }, [id, todoList, setTodoList]);
+
   return (
     <HStack
       borderColor='teal.300'
@@ -17,7 +39,7 @@ const TodoItem = ({ todo: { id, title, completed } }: Props) => {
       justify='space-between'
       spacing={8}>
       <HStack spacing={8}>
-        <Checkbox size='lg' isChecked={completed} />
+        <Checkbox size='lg' isChecked={completed} onChange={handleToggleTodo} />
         <Text
           textDecoration={completed ? "line-through" : undefined}
           color={completed ? "gray.500" : "black"}>
@@ -31,6 +53,7 @@ const TodoItem = ({ todo: { id, title, completed } }: Props) => {
         p={4}
         boxSize={5}
         icon={<DeleteIcon />}
+        onClick={handleDeleteTodo}
       />
     </HStack>
   );
