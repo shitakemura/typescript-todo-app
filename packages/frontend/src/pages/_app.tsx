a1f57176da62e362718a6bc109110eb4592a43fe
shitakemura <shitakemura@gmail.com>
2022-03-09 14:45:57 +0900

    fix: set audience, scope to Auth0Provider

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -14,7 +14,9 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <Auth0Provider
         domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN ?? ""}
         clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID ?? ""}
-        redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIERCT_URL ?? ""}>
+        redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIERCT_URL ?? ""}
+        audience={process.env.NEXT_PUBLIC_AUTH0_API_AUDIENCE}
+        scope={process.env.NEXT_PUBLIC_AUTH0_SCOPE}>
         <ChakraProvider>
           <Component {...pageProps} />
         </ChakraProvider>
